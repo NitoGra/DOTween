282d423bd793b7803d63b835b9d1d399bc463864
ArkNito <b-teme@mail.ru>
2024-06-13 13:37:51 +0300

    минимальное повторение полей

--- Assets/Scripts/Colorer.cs
+++ Assets/Scripts/Colorer.cs
@@ -6,10 +6,8 @@ public class Colorer : DORepeatedTransform
 	[SerializeField] private SpriteRenderer _spriteRenderer;
 	[SerializeField] private Color32 _color;
 
-	private float _duration = 2f;
-
 	private void Start()
 	{
-		_spriteRenderer.DOColor(_color, _duration).SetLoops(Repeats, LoopType);
+		_spriteRenderer.DOColor(_color, Duration).SetLoops(Repeats, LoopType);
 	}
 }
